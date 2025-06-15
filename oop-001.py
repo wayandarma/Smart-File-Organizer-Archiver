3bcce9901da6b6696667736e6b73c8b01190bbaf
Darma | <106679234+wayandarma@users.noreply.github.com>
2025-06-15 23:45:00 +0800

    Add basic OOP example

---
+++ oop-001.py
@@ -0,0 +1,11 @@
+class Flier:
+    def fly(self):
+        print("Flaying High")
+class Swimmer:
+    def swim(fly):
+        print("Swimming smoothly")
+        
+        
+duck = Flier()
+
+duck.fly()
